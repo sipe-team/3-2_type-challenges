562799772b029b47fbf1f7184f7f2278053e794c
do-not-do-that <rl7968@naver.com>
2024-12-27 20:24:13 +0900

    feat : Solve TypeScript Challenge #00004 - easy-pick

---
+++ sipe/do-not-do-that/00004-easy-pick.ts
@@ -0,0 +1,30 @@
+/* _____________ 여기에 코드 입력 _____________ */
+
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  // @ts-expect-error
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+]
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}
